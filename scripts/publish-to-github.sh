f9ada14ebbfb70de772be314abb157542e62de73
Septs <github@septs.pw>
2020-07-18 14:15:20 +0800

    automate publish to github package

--- scripts/publish-to-github.sh
+++ scripts/publish-to-github.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "@dimensiondev:registry=https://npm.pkg.github.com/DimensionDev" > "$HOME/.npmrc"
-echo "//npm.pkg.github.com/:_authToken=${NODE_AUTH_TOKEN}" >> "$HOME/.npmrc"
+echo "//npm.pkg.github.com/:_authToken=${NODE_AUTH_TOKEN}" > "$HOME/.npmrc"
 
 COMMIT_HSAH=$(git rev-parse --short HEAD)
 npm --no-git-tag-version version "0.0.0-$COMMIT_HSAH"
